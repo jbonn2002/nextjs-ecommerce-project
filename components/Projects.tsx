28d388e8932e51f66e62ba6f1ea1b3d9bd1cf080
Jordy <jbonn2002@gmail.com>
2023-06-30 13:52:53 -0700

    updated projects page, added links to buttons and changed project photos

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/Card";
 import Image from "next/image";
 import { Github } from "lucide-react";
+import Link from "next/link";
 
 const Projects = () => {
   return (
@@ -18,7 +19,7 @@ const Projects = () => {
         <CardHeader>
           <CardTitle>Nextjs Prompt Project</CardTitle>
           <CardDescription>
-            Nextjs, Next-Auth, MongoDB, TailwindCSS
+            Next-Auth, MongoDB, Mongoose, TailwindCSS
           </CardDescription>
         </CardHeader>
         <CardContent>
@@ -35,27 +36,39 @@ const Projects = () => {
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">
-            GitHub
-            <div className="ml-2">
-              <Github />
-            </div>
+          <Button asChild variant="outline">
+            <Link
+              href="https://github.com/jbonn2002/Nextjs-prompt-project/"
+              target="_blank"
+            >
+              GitHub
+              <div className="ml-2">
+                <Github />
+              </div>
+            </Link>
           </Button>
-          <Button>Live Site</Button>
+          <Button asChild>
+            <Link
+              href="https://nextjs-prompt-project.vercel.app/"
+              target="_blank"
+            >
+              Live Site
+            </Link>
+          </Button>
         </CardFooter>
       </Card>
       <Card className="w-[400px] mt-5">
         <CardHeader>
-          <CardTitle>Nextjs Prompt Project</CardTitle>
+          <CardTitle>Nextjs Spotify Project</CardTitle>
           <CardDescription>
-            Nextjs, Next-Auth, MongoDB, TailwindCSS
+            Typescript, TailwindCSS, Supabase, Stripe
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex justify-center space-y-1.5">
               <Image
-                src="/prompt-project-screenshot.png"
+                src="/spotifyclonescreenshot.png"
                 alt="test"
                 width={500}
                 height={500}
@@ -65,27 +78,39 @@ const Projects = () => {
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">
-            GitHub
-            <div className="ml-2">
-              <Github />
-            </div>
+          <Button asChild variant="outline">
+            <Link
+              href="https://github.com/jbonn2002/Nextjs-Spotify-Project"
+              target="_blank"
+            >
+              GitHub
+              <div className="ml-2">
+                <Github />
+              </div>
+            </Link>
           </Button>
-          <Button>Live Site</Button>
+          <Button asChild>
+            <Link
+              href="https://nextjs-spotify-project.vercel.app/"
+              target="_blank"
+            >
+              Live Site
+            </Link>
+          </Button>
         </CardFooter>
       </Card>
       <Card className="w-[400px] mt-5">
         <CardHeader>
-          <CardTitle>Nextjs Prompt Project</CardTitle>
+          <CardTitle>Nextjs Reddit Project</CardTitle>
           <CardDescription>
-            Nextjs, Next-Auth, MongoDB, TailwindCSS
+            Typescript, React-Query, Prisma, Zod
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex justify-center space-y-1.5">
               <Image
-                src="/prompt-project-screenshot.png"
+                src="/redditcloness.png"
                 alt="test"
                 width={500}
                 height={500}
@@ -95,27 +120,32 @@ const Projects = () => {
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">
-            GitHub
-            <div className="ml-2">
-              <Github />
-            </div>
+          <Button asChild variant="outline">
+            <Link
+              href="https://github.com/jbonn2002/nextjs-reddit-clone"
+              target="_blank"
+            >
+              GitHub
+              <div className="ml-2">
+                <Github />
+              </div>
+            </Link>
           </Button>
-          <Button>Live Site</Button>
+          <Button asChild>
+            <Link href="">Live Site</Link>
+          </Button>
         </CardFooter>
       </Card>
       <Card className="w-[400px] mt-5">
         <CardHeader>
-          <CardTitle>Nextjs Prompt Project</CardTitle>
-          <CardDescription>
-            Nextjs, Next-Auth, MongoDB, TailwindCSS
-          </CardDescription>
+          <CardTitle>React Hoobank Clone</CardTitle>
+          <CardDescription>React, Tailwind</CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex justify-center space-y-1.5">
               <Image
-                src="/prompt-project-screenshot.png"
+                src="/HoobankSs.png"
                 alt="test"
                 width={500}
                 height={500}
@@ -125,13 +155,22 @@ const Projects = () => {
           </div>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button variant="outline">
-            GitHub
-            <div className="ml-2">
-              <Github />
-            </div>
+          <Button asChild variant="outline">
+            <Link
+              href="https://github.com/jbonn2002/first-react-project"
+              target="_blank"
+            >
+              GitHub
+              <div className="ml-2">
+                <Github />
+              </div>
+            </Link>
           </Button>
-          <Button>Live Site</Button>
+          <Button asChild>
+            <Link href="https://reacthoobankclone.netlify.app/" target="_blank">
+              Live Site
+            </Link>
+          </Button>
         </CardFooter>
       </Card>
     </div>
