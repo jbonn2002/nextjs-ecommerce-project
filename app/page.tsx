6254d315ad7cb8c69d184bd739e95dbf2c2d6c63
Jordy <jbonn2002@gmail.com>
2023-06-30 16:24:30 -0700

    added react three fiber

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import ThreejsSection from "@/components/ThreejsSection";
+
 export default function Home() {
   return (
     <section className="w-full flex-center flex-col">
@@ -6,9 +8,22 @@ export default function Home() {
         <br className="max-md:hidden" />
         <span className="orange_gradient">is Jordan Bonnaire </span>
       </h1>
-      <p className="desc text-center">
-        Promptopia is an open-source AI prompting tool for the modern world to
-        discover, create and share creative prompts
+      <hr className="w-[75%] h-px my-6" />
+      <ThreejsSection />
+      <hr className="w-[75%] h-px my-6" />
+      <p className="desc text-center text-white">
+        My name is jordan bonnaire, as a front-end web developer, I am dedicated
+        to creating visually stunning and user-friendly websites that engage and
+        captivate users. My expertise includes proficiency in HTML, CSS,
+        JavaScript, and various front-end libraries and frameworks such as
+        Three.js, React, and Gsap. I am committed to understanding user needs
+        and designing intuitive user interfaces that provide a seamless
+        experience across all devices. My attention to detail and ability to
+        think creatively allows me to create beautiful and functional designs
+        that enhance the overall user experience. I am passionate about staying
+        up-to-date with the latest front-end development trends and techniques
+        to ensure that my clients receive the most innovative and effective
+        solutions.
       </p>
     </section>
   );
---
+++ components/ThreejsSection.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Mesh } from "three";
+
+const ThreejsSection = () => {
+  function Box(props: any) {
+    const ref = useRef<Mesh>(null!);
+    // Hold state for hovered and clicked events
+    const [hovered, hover] = useState(false);
+    const [clicked, click] = useState(false);
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += delta));
+    // Return the view, these are regular Threejs elements expressed in JSX
+    return (
+      <mesh
+        {...props}
+        ref={ref}
+        scale={clicked ? 1.5 : 1}
+        onClick={(event) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+      >
+        <boxGeometry args={[5, 5, 5]} />
+        <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      </mesh>
+    );
+  }
+
+  return (
+    <div className="w-full h-full">
+      <Canvas>
+        <PerspectiveCamera
+          makeDefault
+          fov={75}
+          near={0.1}
+          far={1000}
+          position={[0, 0, 10]}
+        />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box position={[-5, 0, 0]} />
+        <Box position={[5, 0, 0]} />
+      </Canvas>
+    </div>
+  );
+};
+
+export default ThreejsSection;
